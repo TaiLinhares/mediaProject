96509c3d5e90829f1512ccdfb4172413bc36dc76
niz11 <nizan.goldstein@gmail.com>
2018-06-01 16:25:11 +0200

    Correcting german letters and building page with jquery

---
+++ public/js/courses/EinPro.js
@@ -0,0 +1,7 @@
+var EinPro = {
+    name: 'Einführung in die Programmierung',
+    lp: '6 LP',
+    sws: '4 SWS',
+    turnus: 'WiSe',
+    link: 'https://moseskonto.tu-berlin.de/moses/modultransfersystem/bolognamodule/beschreibung/anzeigen.html?number=40017&version=5&sprache=1'
+} 
